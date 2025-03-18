6df4b9bbaea451d164211105caeadc2390560b74
Your Name <you@example.com>
2025-03-18 14:28:26 +0800

    Assignment: Operators I

---
+++ Java/Assignment Operators I/modulusOperatorMHCO.java
@@ -0,0 +1,12 @@
+public class modulusOperatorMHCO {
+    public static void main(String[] args) {
+        int a = 5, b = 2;
+        int result = (a % b);
+        if (result == 0) {
+            System.out.println("No remainder");
+        }
+        else { 
+            System.out.println("Remainder is: " + result);
+        }
+    }
+}
---
+++ Java/Assignment Operators I/relationalOperatorsMHCO.java
@@ -0,0 +1,16 @@
+public class relationalOperatorsMHCO {
+    public static void main(String[] args) {
+        int x = 9, y = 2;
+
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+
+        if (x < 10) {
+            System.out.println(x + " is less than to 10");
+        }
+    }
+}
