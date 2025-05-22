8d91c6492565fcf983ecc1691c71134f930e68db
Your Name <you@example.com>
2025-05-22 14:38:11 +0800

    jsx activities

---
+++ jsx-intro/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    // Logic Section: Define your variables here
+    const greeting = "Hello, JSX!";
+    const name = "React Developer's";
+    const year = 2025;
+
+    return (
+        <>
+            {/* Render Section: Use JSX expressions to insert variables */}
+            <h1>{greeting} Welcome to the {name} Journey in {year}</h1>
+        </>
+    )
+}
+
+export default App
